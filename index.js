e69faefbee81341e89674a332460bf5ff45e600a
Mark <74011034+thecodehippo@users.noreply.github.com>
2024-12-26 21:54:40 +0000

    changes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const animationDelays = ["animationDelay0", "animationDelay5", "animationDelay10
 const animationTiming = ["animationTiming1", "animationTiming2", "animationTiming3", "animationTiming4", "animationTiming5", "animationTiming6", "animationTiming7", "animationTiming8", "animationTiming9", "animationTiming10"];
 const viewportHeight = window.innerHeight;
 const viewportWidth = window.innerWidth;
-const helper = document.getElementById("helper"); 
 
 //event listeners
 const spinButton = document.getElementById("spinButton");
@@ -24,13 +23,11 @@ function loadReels() {
 }
 
 function spin() {
-    helper.innerText = ""
     animatedDivs.forEach((d, key) => {
         d.animationDirection = false //Math.random() < 0.5;
         d.animationDelay = animationDelays[4] //animationDelays[Math.floor(Math.random() * animationDelays.length)];
         d.animationTiming = animationTiming[2] //animationTiming[Math.floor(Math.random() * animationTiming.length)];
         spinCount++
-        helper.innerText = helper.innerText + key + d.animationDirection + d.animationDelay + d.animationTiming + viewportHeight + viewportWidth + "\n";
         fillReel(d, numSymbols, key, d.animationDirection)
         d.animationDirection ? d.classList.add("active", d.animationDelay, d.animationTiming) : d.classList.add("reverse", d.animationDelay, d.animationTiming);
         d.animationDirection ? d.setAttribute("style", "transform: translateY(0)") : d.setAttribute("style", `transform: translateY(-${(viewportHeight / 100 * 10) * 58}px`);
